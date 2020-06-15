63b470a719195b6c73d89abc3ad3becce8c8d0c1
Piotr <pmilewski007@gmail.com>
2020-06-15 17:08:07 +0200

    fix

--- zamianaSystemow.cpp
+++ zamianaSystemow.cpp
@@ -7,6 +7,6 @@ void z10naDowolny(int n, int p){
 }
 
 int zDowolnegoNa10(int d, string x, int p){
-	if(d-1==0) return int(x[0]-'7')<10 ? int(x[0]-'0') : int(x[0]-'7');
-	return int(x[d-1]-'7')<10 ? p*zDowolnegoNa10(d-1,x,p)+int(x[d-1]-'0') : p*zDowolnegoNa10(d-1,x,p)+int(x[d-1]-'7');
+	if(d-1==0) return int(x[0]-'A'+10)<10 ? int(x[0]-'0') : int(x[0]-'A'+10);
+	return int(x[d-1]-'A'+10)<10 ? p*zDowolnegoNa10(d-1,x,p)+int(x[d-1]-'0') : p*zDowolnegoNa10(d-1,x,p)+int(x[d-1]-'A'+10);
 }
